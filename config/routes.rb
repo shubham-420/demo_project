fb0f70da1aabf0590ee3dbdc75893bc4db56b749
shubham-420 <smehta@gammastack.com>
2020-02-12 18:09:42 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  # devise_for :users
+  
+  devise_scope :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  get 'welcome/index'
+  root 'welcome#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
