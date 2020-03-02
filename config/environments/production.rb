5a70212867d7a0686c2cea6ac893009ed3176c28
shubham-420 <smehta@gammastack.com>
2020-03-02 19:10:40 +0530

    Fix host for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000'
+  #host = 'localhost:3000'
   config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
 
 
