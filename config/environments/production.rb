8025441a5c09b419024c3da75de3030039d5f532
shubham-420 <smehta@gammastack.com>
2020-03-03 12:35:03 +0530

    email changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => 'mehtashubham543@gmail.com',
-    :password => '9981733235',
+    :user_name => 'chatbookteam@gmail.com',
+    :password => 'chat@book123',
     :domain => 'herokuapp.com',
     :address => 'smtp.gmail.com',
     :port => 587,
